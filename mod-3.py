99b9eeaaa0d6ab366648de306acae2b654eabe11
mary ann <maryannbernardo212@gmail.com>
2022-05-28 02:05:37 +0800

    Excercise 3.4

---
+++ mod-3.py
@@ -0,0 +1,8 @@
+fileText = open("pythonQuestion.txt", "w")
+fileText.write("I like to learn in the future data science in python.")
+
+fileText = open("pythonQuestion.txt", "a")
+fileText.write("\nMaking a project.")
+fileText.write("\nDo coding challanges or building a python project.")
+fileText.write("\nTo become a Python developer and data analyst in the future.")
+fileText.close()
